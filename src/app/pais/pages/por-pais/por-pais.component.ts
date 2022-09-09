686ce8f73a50607f75a97a6f2757cf632abde63f
Arnaldo Jose Bonillo Berrios <bonillaarnaldo@gmail.com>
2022-09-09 13:06:18 -0430

    Video 109 llenar la tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,16 +10,26 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent  {
 
-  termino: string = "";
+  termino : string = "";
+  hayError: boolean = false;
+  paises  : Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
  buscar(){
+  this.hayError = false;
   console.log(this.termino);
+
   this.paisService.buscarPais(this.termino)
-  .subscribe(resp => {
-    console.log(resp);
+  .subscribe(paises => {
+    console.log(paises);
+    this.paises = paises;
+
+  }, (err) => {
+    this.hayError = true;
+    this.paises = [];
   });
+
  }
 
 }
